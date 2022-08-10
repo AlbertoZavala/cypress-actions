38cbfe93de372293d3587ef205099e11bfb13568
Alberto Zavala <jose.zavala@lcliquidezcorporativa.com.mx>
2022-08-10 10:51:01 -0700

    Remove test

--- cypress/e2e/headers.cy.js
+++
@@ -1,5 +0,0 @@
-describe("Probando headers", function(){
-  it("debe de validar el header y el content type", function(){
-    cy.request('employees').its('headers').its('content-type').should('include', 'application/json');
-  });
-});
