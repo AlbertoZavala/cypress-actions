adf6cbc5189b12a3e2983a371150e5dc27ce8c29
Alberto Zavala <jose.zavala@lcliquidezcorporativa.com.mx>
2022-08-16 19:48:48 -0700

    Adding Amazon Test

--- cypress/e2e/amazon.test.cy.js
+++ cypress/e2e/amazon.test.cy.js
@@ -5,9 +5,17 @@ describe("Amazon Test", function(){
     cy.get('#nav-logo-sprites', {timeout: 10000}).should('be.visible');
   });
 
-  it("Loggin", function(){
-    const button = cy.get('#nav-signin-tooltip > .nav-action-button > .nav-action-inner', {timeout: 10000}).should('be.visible');    
-    button.click();
+  it('Adding a product', function(){
+    cy.get('#twotabsearchtextbox').type('Iphone');
+    cy.get('#nav-search-submit-button').click();
+    
+    cy.get('span').contains('RESULTADOS').should('be.visible');
+    
+    //const resultList = cy.get('.s-result-list');
+    
 
   });
+
+  
+  
 });
