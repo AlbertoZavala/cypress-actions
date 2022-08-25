26cd553f85d742e0c5525cd3ed611d4ff4fdde3e
Alberto Zavala <jose.zavala@lcliquidezcorporativa.com.mx>
2022-08-25 09:47:40 -0700

    getFromJson Added

--- cypress/e2e/03-getFromJson/getFromJson.cy.js
+++ cypress/e2e/03-getFromJson/getFromJson.cy.js
@@ -5,8 +5,7 @@
 // 5. Add locators in json file
 // 2. Excecute the command on beforeEach.
 
-
-describe("Amazon Test", function(){
+describe("Get From Json Test", function(){
 
   // Added because Amazon site is throwing an error that
   // don't let me go further at the test.
@@ -14,45 +13,33 @@ describe("Amazon Test", function(){
     return false;
   });
 
+  before(function(){
+    cy.fixture('amazon.json').then((locators) => {
+      this.locators = locators;
+    });
+  });
   
   beforeEach(function(){
-    cy.visit('https://www.amazon.com.mx');    
-
-    cy.fixture('amazon.json')
-      .then((locators) => {
-        this.locators = locators;
-      })
+    cy.goToProduct();    
   });
 
   
-  it('Adding a product to wish list', function(){
-    
-    cy.get(this.locators.logoLocator).should('be.visible');
-    cy.get(this.locators.searchBox).type(this.locators.searchedIteme);
-    cy.get(this.locators.searchBoxButton).click();
-    
-    cy.get('span').contains(this.locators.textResultHeader).should('be.visible');
-    
-    // cy.get('span').filter(`:contains(${this.locators.selectedItem})`).eq(0).should('be.visible').click({force: true});
-    cy
-      .get('[data-component-type="s-search-results"]')
-      .get('[data-index="1"]').should('be.visible')
-      .get('href').should('be.visible')
+  it('Adding a product to wish list', function(){    
+    // cy.get(this.locators.logoLocator).should('be.visible');
+    // cy.get(this.locators.searchBox).type(this.locators.searchedIteme);
+    // cy.get(this.locators.searchBoxButton).click();    
+    // cy.get('span').contains(this.locators.textResultHeader).should('be.visible');
 
-    //#search > div.s-desktop-width-max.s-desktop-content.s-opposite-dir.sg-row > div.s-matching-dir.sg-col-16-of-20.sg-col.sg-col-8-of-12.sg-col-12-of-16 > div > span:nth-child(4) > div.s-main-slot.s-result-list.s-search-results.sg-row > div:nth-child(4)
+    // cy.get(this.locators.resultList).should('be.visible')
+    //   .find(this.locators.firstElementContainer).should('be.visible')      
+    //   .find(this.locators.firstElementImage).should('be.visible')
+    //   .click()
+
+    // cy.get(this.locators.wishListButton).should('be.visible').click()
+    cy.get(this.locators.wishListButton).should('be.visible').click()
+
     
-
-    // const selectedProduct = elements.filter(element => elements.includes(element))
-    // cy.log(selectedProduct);
     
-
-    //products[0].should('be.visible').click();
-        
-    // cy.get(this.locators.addToCartButton).should('be.visible').click();
-
-    // cy.get('#attachSiNoCoverage').should('be.visible').click();
-    // cy.get('#attach-sidesheet-view-cart-button').should('be.visible').click();
-  });
-  
+  });  
 });
 
